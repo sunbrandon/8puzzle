af5ddafeb247e4f437dec656b1d3b5b30b79d708
sunbrandon <sunabrandon@gmail.com>
2025-02-07 17:45:10 -0800

    Implement null finder

--- main.cpp
+++ main.cpp
@@ -17,6 +17,61 @@ struct PuzzleState {
     string path;
 };
 
+bool isGoalState(const PuzzleState& state) {
+    return state.board == GOAL_STATE;
+}
+
+void findEmptyTile(const vector<vector<int>>& board, int& row, int& col) {
+    if (board[0][0] == 0) { 
+        row = 0; 
+        col = 0; 
+        return; 
+    }
+    if (board[0][1] == 0) { 
+        row = 0; 
+        col = 1; 
+        return; 
+    }
+    if (board[0][2] == 0) { 
+        row = 0; 
+        col = 2; 
+        return; 
+    }
+    if (board[1][0] == 0) { 
+        row = 1; 
+        col = 0; 
+        return; 
+    }
+    if (board[1][1] == 0) { 
+        row = 1; 
+        col = 1; 
+        return; 
+    }
+    if (board[1][2] == 0) { 
+        row = 1; 
+        col = 2; 
+        return; 
+    }
+    if (board[2][0] == 0) { 
+        row = 2; 
+        col = 0; 
+        return; 
+    }
+    if (board[2][1] == 0) { 
+        row = 2; 
+        col = 1; 
+        return; 
+    }
+    if (board[2][2] == 0) { 
+        row = 2; 
+        col = 2; 
+        return; 
+    }
+    // If board is invalid return non possible values
+    row = -1; 
+    col = -1;
+}
+
 int main() {
     PuzzleState initialState;
     initialState.board = {
@@ -33,6 +88,16 @@ int main() {
             cout << initialState.board[i][j] << " ";
         }
         cout << endl;
+    }
+
+int row, col;
+    findEmptyTile(initialState.board, row, col);
+    cout << "Empty tile is at: (" << row << ", " << col << ")" << endl;
+
+    if (isGoalState(initialState)) {
+        cout << "The initial state is already the goal state!" << endl;
+    } else {
+        cout << "The initial state is not the goal state." << endl;
     }
 
     return 0;
